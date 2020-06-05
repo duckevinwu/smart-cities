3d554fd611ae0efce710c70f3e17e20662680a42
Kevin <kevinwu97@gmail.com>
2020-06-05 11:29:05 -0500

    set up server files

---
+++ index.js
@@ -0,0 +1,28 @@
+const bodyParser = require('body-parser');
+const express = require('express');
+var routes = require("./routes.js");
+const cors = require('cors');
+
+const app = express();
+
+app.use(cors());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
+
+/* ---------------------------------------------------------------- */
+/* ------------------- Route handler registration ----------------- */
+/* ---------------------------------------------------------------- */
+
+/* ---- (Dashboard) ---- */
+// The route localhost:8081/people is registered to the function
+// routes.getAllPeople, specified in routes.js.
+app.get('/people', routes.getAllPeople);
+
+/* ---- Part 2 (FindFriends) ---- */
+// TODO: (2) - Add route '/friends/:login' for the functionality of FindFriends page
+app.get('/friends/:login', routes.getFriends); // Hint: Replace () => {} with the appropriate route handler in routes.js.
+
+
+app.listen(8081, () => {
+	console.log(`Server listening on PORT 8081`);
+});
---
+++ routes.js
@@ -0,0 +1,48 @@
+var config = require('./db-config.js');
+var mysql = require('mysql');
+
+config.connectionLimit = 10;
+var connection = mysql.createPool(config);
+
+/* -------------------------------------------------- */
+/* ------------------- Route Handlers --------------- */
+/* -------------------------------------------------- */
+
+/* ---- (Dashboard) ---- */
+function getAllPeople(req, res) {
+  var query = `
+    SELECT login, name, birthyear
+    FROM Person;
+  `;
+  connection.query(query, function(err, rows, fields) {
+    if (err) console.log(err);
+    else {
+      res.json(rows);
+    }
+  });
+};
+
+/* ---- Part 2 (FindFriends) ---- */
+function getFriends(req, res) {
+  var inputLogin = req.params.login;
+
+  // TODO: (3) - Edit query below
+  var query = `
+    SELECT f.friend, p.name
+    FROM Friends f JOIN Person p ON f.friend=p.login
+    WHERE f.login='${inputLogin}'
+  `;
+  connection.query(query, function(err, rows, fields) {
+    if (err) console.log(err);
+    else {
+      console.log(rows);
+      res.json(rows);
+    }
+  });
+};
+
+// The exported functions, which can be accessed in index.js.
+module.exports = {
+  getAllPeople: getAllPeople,
+  getFriends: getFriends
+}
