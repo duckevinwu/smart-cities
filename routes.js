46c1d3ab2603b37c3e508ed971b240955de47c3c
Kevin <kevinwu97@gmail.com>
2020-10-03 16:51:08 -0500

    configure server for aws db

--- routes.js
+++ routes.js
@@ -9,7 +9,8 @@ var config = {
   host: process.env.HOST,
   user: process.env.USER,
   password: process.env.PASSWORD,
-  database: process.env.DATABASE
+  database: process.env.DATABASE,
+  port: process.env.DB_PORT
 }
 
 var randToken = require('rand-token');
